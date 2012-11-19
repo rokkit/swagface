6384d6793f7b79844d5dfa69ea1673cdb24d888d
rokkit <fss92@pochta.ru>
2012-11-19 18:44:36 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swagtest::Application.config.secret_token = '553acb2d5e8b995de0b90b0cb07d8be668fe96240568e20b28e3a42cc724779ef8cb21dabb3c6d36410a4132661ffde44e4861377dd1f22ab9c3b0fbe6beb36d'
